425dccc99da7143cb169642c0f086082f969a14b
riahi semah <riahisemah817@gmail.com>
2023-01-19 11:05:42 +0100

    first commit

---
+++ boards/views.py
@@ -0,0 +1,6 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("hello my name is semah This my first website ")
+
+# Create your views here.
